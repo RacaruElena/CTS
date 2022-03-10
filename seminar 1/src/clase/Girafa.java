07b852c9cde6483ca5142543603a5fe4b362a1fd
Elena Racaru <85835406+Racaru@users.noreply.github.com>
2022-03-10 11:42:15 +0200

    Update

---
+++ seminar 1/src/clase/Girafa.java
@@ -0,0 +1,12 @@
+package clase;
+
+public class Girafa extends Animal{
+    public Girafa(String nume) {
+        super(nume);
+    }
+
+    @Override
+    public void mananca(String tipMancare) {
+        System.out.println("Girafa " + getNume() + "mananca" + tipMancare);
+    }
+}
